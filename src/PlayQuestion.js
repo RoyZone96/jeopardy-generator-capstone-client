3e8a734b8a480756666f7b4e15c8720d62d7e365
Briceson Roy <royzone3@ymail.com>
2020-08-01 18:55:56 -0400

    added event

--- src/PlayQuestion.js
+++ src/PlayQuestion.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import ApiContext from './ApiContext'
 import { Data } from "./dummy-store"
 import { Link } from "react-router-dom"
 
@@ -8,56 +7,39 @@ export default class PlayQuestion extends Component {
   
     state = {
             answer: {
-                show: false
+                isShown: false
             }
         }
     
 
-    
-    // static contextType = ApiContext 
 
-    toggleState = event => {
+    toggleState(event){
         this.setState({
-          answer: { show: !this.state.show }
+          answer: { isShown: !this.state.isShown }
         });
       }
       
     
-      handleClick = (event) => {
-        event.preventDefault();
-       
-        const { answer } = event.target
-        console.log(answer.value)
-        console.log(this.props.match.params.value)
-
-         
-      }
-    
-
 
     render() {
-        
-
-        let { category, value, id, show} = this.props.match.params
+        let { category, value, id, isShown} = this.props.match.params
         category = category.replace( /-/g, " ")
         let cat = Data.data[id-1].questions[value/100-1]
-        console.log(cat)
         return (
             <div>
                 <Link to='/play'><button type="button">BACK</button></Link>
-                <form onSubmit={ this.handleSubmit }>
+                <div>
                     <div className="wrapper">
                         <p>{cat.question}
                     </p>
                     </div>
                     <div className="wrapper">
-                        <label htmlFor="Answer"> What is </label>
-                        <input type="text" placeholder="Answer" name="answer" required />
+                        <p> What is <p className="answer">{cat.answer}</p> ?</p>
                     </div>
                     <div>
                         <button type="button" onClick={this.toggleState}>Reveal</button>
                     </div>
-                </form>
+                </div>
             </div >
         )
     }
