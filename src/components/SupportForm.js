217fe75601872fd658bce04fc751c52edd811640
Briceson Roy <royzone3@ymail.com>
2020-11-19 23:05:40 -0500

    fixing support form

--- src/components/SupportForm.js
+++ src/components/SupportForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import config from '../config'
+import ApiContext from '../ApiContext'
+
 
 export default class SupportForm extends Component {
     constructor(props) {
@@ -14,30 +17,59 @@ export default class SupportForm extends Component {
     }
 
 
+
     handleChange(event) {
         this.setState({ value: event.target.value })
     }
 
     handleSubmit(event) {
-       
         event.preventDefault();
-         alert('Thank you for your submission. The admin will look over this as soon as possible')
-         const { email, subject, content } = event.target;
-         console.log('email:', email.value, 'subject:', subject.value, 'content', content.value);
+
+        const newSupport = {
+            email: this.state.email.value,
+            subject: this.state.subject.value,
+            content: this.state.content.value
+        }
+
+        console.log(newSupport)
+
+        fetch(`${config.API_ENDPOINT}/supports`,
+            {
+                method: 'POST',
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify(newSupport),
+            })
+            .then(res => {
+                if (!res.ok)
+                    return res.json().then(e => Promise.reject(e))
+                return res.json()
+            })
+            .then(response =>
+                this.context.addQuestion(response),
+                console.log(ApiContext))
+            .then(
+                console.log(newSupport),
+                this.props.history.push('/')
+            )
+            .catch(error => {
+                alert(error.message)
+            })
+        alert('Thank you for your submission. The admin will look over this as soon as possible')
+
     }
 
     render() {
         return (
             <div>
-                <form onSubmit = { this.handleSubmit }>
+                <form onSubmit={this.handleSubmit}>
                     <div className="wrapper">
-                        <input type="email" className="email" name="email" placeholder="E-mail" required />
+                        <input type="text" value = {this.state.email.value} onChange={this.handleChange} className="email" name="email" placeholder="E-mail" required />
                     </div>
                     <div className="wrapper">
-                        <input type="text" className="subject" name="subject" placeholder="Subject" required />
+                        <input type="text" value = { this.state.subject.value} onChange={this.handleChange} className="subject" name="subject" placeholder="Subject" required />
                     </div>
                     <div className="wrapper">
-                        <textarea  name="content" onChange={this.handleChange} className="content" placeholder="Your content here" required />
+                        <textarea name="content" value = {this.state.content.value} onChange={this.handleChange} className="content" placeholder="Your content here" required />
                     </div>
                     <div className="wrapper">
                         <button type="submit">
