4d8a1010fa9954946f30da51701ed348ab81a009
Briceson Roy <royzone3@ymail.com>
2021-03-21 15:21:59 -0400

    changed connection point

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 
-    'http://localhost:8000/api',
-    // API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    // "http://localhost:3000/api",
+    // API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://jeapordy-generator-capstone.herokuapp.com/api',
     TOKEN_KEY: 'jeopardy-generator-capstone-server'
 }
