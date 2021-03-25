9121e468f3e0b1496426dabe169d016ebca60c74
Briceson Roy <royzone3@ymail.com>
2021-03-25 12:47:02 -0400

    changed base url to endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     // API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://jeapordy-generator-capstone.herokuapp.com/api',
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'https://jeapordy-generator-capstone.herokuapp.com/api',
     TOKEN_KEY: 'jeopardy-generator-capstone-server'
 }
